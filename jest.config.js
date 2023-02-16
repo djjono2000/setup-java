4e3101f4a62def38cb6b87f98f2e4bf27e99fa7d
djjono2000 <76519663+djjono2000@users.noreply.github.com>
2023-02-17 00:59:03 +1030

    love it

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,13 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
   transform: {
+
     '^.+\\.ts$': 'ts-jest'
   },
-  verbose: true
-}
+for (const key in object) {
+  if (Object.hasOwnProperty.call(object, key)) {
+    const element = object[key];
+      verbose: true
+    
+  }
+}}
